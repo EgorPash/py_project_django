f829f2a953c5465a05f5e42d22a31f96c21099c1
egorpash <2021104302@pnu.edu.ru>
2024-10-05 18:56:22 +0200

    The project is updated by new templates.

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,5 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('contact/', views.contact_view, name='contact'),
+    path('product/<int:pk>/', views.product_detail_view, name='product_detail'),
 ]
