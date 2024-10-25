230b709e8f58022ff3ceea132e1a97ea65a3fa24
egorpash <2021104302@pnu.edu.ru>
2024-10-25 03:04:30 +0200

    Попытка добавления страницы регистрации в данный проект.

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-class CustomUserCreationForm(UserCreationForm):
+class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
--- users/views.py
+++ users/views.py
@@ -1,54 +1,13 @@
-import random
 from django.views.generic import CreateView
-from django.urls import reverse_lazy
-from django.core.mail import send_mail
-from .forms import CustomUserCreationForm
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.hashers import make_password
-from django.http import HttpResponse
-from .models import User
-from django.views.generic import TemplateView
+from rest_framework.reverse import reverse_lazy
 
-class UserPageView(TemplateView):
-    template_name = 'users/user_page.html'
+from users.forms import UserRegisterForm
+from users.models import User
+
 
 class RegisterView(CreateView):
     model = User
-    form_class = CustomUserCreationForm
+    form_class = UserRegisterForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('login')
-
-    def form_valid(self, form):
-        user = form.save()
-        # Отправка письма для верификации почты
-        send_mail(
-            'Welcome!',
-            'Thank you for registering.',
-            'from@example.com',
-            [user.email],
-            fail_silently=False,
-        )
-        return super().form_valid(form)
-
-class PasswordResetView(CreateView):
-    template_name = 'users/password_reset.html'
-
-    def post(self, request, **kwargs):
-        email = request.POST.get('email')
-        try:
-            user = User.objects.get(email=email)
-            new_password = str(random.randint(100000, 999999))
-            user.password = make_password(new_password)
-            user.save()
-
-            send_mail(
-                'Сброс пароля',
-                f'Ваш новый пароль: {new_password}',
-                'from@example.com',
-                [email],
-                fail_silently=False,
-            )
-            return HttpResponse('Пароль сброшен! Проверьте почту.')
-        except User.DoesNotExist:
-            return HttpResponse('Пользователь с такой почтой не найден.')
+    success_url = reverse_lazy('users:login')
 
